04ec2713b70dd15016c2ed42d7d985cde94bb25b
bashalir <bachir@sahali.fr>
2020-05-19 14:05:39 +0000

    Fix model ads & users + Fix storages route

--- src/routes/ads.js
+++ src/routes/ads.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 const adsController = require('../controllers/ads_controller');
-const verifyToken = require('../middlewares/verify_token');
+// const verifyToken = require('../middlewares/verify_token');
 
 // router.use('*', verifyToken);
 router.post('/', async (request, response) => {
